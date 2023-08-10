28c7553d483bc2959a6fc90f459adf36eb19ba83
Cinthia Sanchez Gallardo <ceenti@MacBook-Pro-de-Cinthia.local>
2023-08-10 14:44:59 -0600

    Initial setup

---
+++ pages/categories/index.tsx
@@ -0,0 +1,36 @@
+import { MuiListInferencer } from "@refinedev/inferencer/mui";
+import { GetServerSideProps } from "next";
+import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { authProvider } from "src/authProvider";
+
+import { inferencerPredefinedMeta } from "src/inferencerPredefinedMeta";
+
+export default function CategoryList() {
+  return <MuiListInferencer meta={inferencerPredefinedMeta} />;
+}
+
+export const getServerSideProps: GetServerSideProps<{}> = async (context) => {
+  const { authenticated, redirectTo } = await authProvider.check(context);
+
+  const translateProps = await serverSideTranslations(context.locale ?? "en", [
+    "common",
+  ]);
+
+  if (!authenticated) {
+    return {
+      props: {
+        ...translateProps,
+      },
+      redirect: {
+        destination: `${redirectTo}?to=${encodeURIComponent("/categories")}`,
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      ...translateProps,
+    },
+  };
+};
